88e14baa57a0d189dcc5a5a82959e3e6826d5a2f
Nahuel Ripoll <nahuelripoll33@gmail.com>
2024-10-03 20:57:31 -0300

    Utilizando clave privada

--- src/weather.js
+++ src/weather.js
@@ -11,26 +11,29 @@ async function getWeather(city) {
     throw new Error("City is required");
   }
   
-  const url = `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`; // URL completa con la clave de la API y la ciudad
+  const url = `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`;
 
   try {
+    console.log(`Requesting URL: ${url}`);
     const response = await fetch(url);
+    
     if (!response.ok) {
+      console.log(`Error: ${response.status} - ${response.statusText}`);
       throw new Error(`Error fetching weather data: ${response.statusText}`);
     }
+
     const data = await response.json();
     return {
-      temperature: data.current.temp_c,
-      description: data.current.condition.text,
-      city: data.location.name,
-      country: data.location.country
+      temperature: data.main.temp,
+      description: data.weather[0].description,
+      city: data.name,
+      country: data.sys.country
     };
   } catch (error) {
+    console.error(`Unable to get weather data: ${error.message}`);
     throw new Error(`Unable to get weather data: ${error.message}`);
   }
 }
-
-console.log(API_KEY); // Verifica si la clave API está siendo leída correctamente
 
 
 export default getWeather;
